ac74df00dd68ca3ade4a47631851a2ed2acab424
pingpongsneak <matthew.hellyer@pm.me>
2022-12-27 21:59:31 +0000

    refactor: remove small PoW

--- miner/Ledger/Blockchain.cs
+++ miner/Ledger/Blockchain.cs
@@ -2,7 +2,6 @@
 // To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-nd/4.0
 
 using System;
-using System.Diagnostics;
 using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
@@ -80,17 +79,14 @@ public class Blockchain : IBlockchain, IDisposable
                 Curve.decodePrivatePoint(_sessionService.KeyPair.PrivateKey), kernel);
             var verifyVrfSignature = Crypto.GetVerifyVrfSignature(
                 Curve.decodePoint(_sessionService.KeyPair.PublicKey, 0), kernel, calculatedVrfSignature);
-            var solution = await SolutionAsync(calculatedVrfSignature, kernel);
-            if (solution.Sol == 0) return null;
-            var nonce = await GetNonceAsync(verifyVrfSignature, Bits(solution.Sol));
-            if (nonce.Length == 0) return null;
+            var slow = await GetNonceAsync(verifyVrfSignature, BitTime);
+            if (slow is { Nonce.Length: 0 }) return null;
             blockMinerProof = new BlockMinerProof
             {
                 Hash = block.Hash,
                 Address = _sessionService.Address,
                 Locktime = lockTime,
-                Nonce = nonce,
-                Solution = solution.Sol,
+                Nonce = slow.Nonce,
                 LocktimeScript = new Script(Op.GetPushOp(lockTime), OpcodeType.OP_CHECKLOCKTIMEVERIFY).ToString()
                     .ToBytes(),
                 PublicKey = _sessionService.KeyPair.PublicKey,
@@ -118,67 +114,7 @@ public class Blockchain : IBlockchain, IDisposable
         diff = diff == 0 ? 1 : diff;
         return (int)diff;
     }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="vrfBytes"></param>
-    /// <param name="kernel"></param>
-    /// <returns></returns>
-    private static async Task<Solution> SolutionAsync(byte[] vrfBytes, byte[] kernel)
-    {
-        return await Task.Factory.StartNew(() =>
-        {
-            var sw = new Stopwatch();
-            var sol = new Solution();
-            try
-            {
-                long itr = 0;
-                var target = new BigInteger(1, Hasher.Hash(vrfBytes).HexToByte());
-                var hashTarget = new BigInteger(1, kernel);
-                var hashTargetValue = new BigInteger((target.IntValue / hashTarget.BitCount).ToString()).Abs();
-                var hashWeightedTarget = new BigInteger(1, kernel).Multiply(hashTargetValue);
-                sw.Start();
-                while (true)
-                {
-                    if (sw.ElapsedMilliseconds > StopSolutionInMilliseconds)
-                    {
-                        itr = 0;
-                        break;
-                    }
-
-                    var weightedTarget = target.Multiply(BigInteger.ValueOf(itr));
-                    if (hashWeightedTarget.CompareTo(weightedTarget) <= 0)
-                    {
-                        sw.Stop();
-                        break;
-                    }
-
-                    if (itr == long.MaxValue)
-                    {
-                        itr = 0;
-                        break;
-                    }
-
-                    itr++;
-                }
-
-                if (sw.IsRunning) sw.Stop();
-                sol.Sol = (ulong)itr;
-                if (sol.Sol == 0) return sol;
-                var seconds = sw.ElapsedMilliseconds * 10 ^ -3;
-                sol.HashRatePerSecond = seconds < 0 ? (long)sol.Sol : (long)(sol.Sol / (ulong)seconds);
-                return sol;
-            }
-            catch (Exception)
-            {
-                sw.Stop();
-            }
-
-            return sol;
-        }, TaskCreationOptions.LongRunning).ConfigureAwait(false);
-    }
-
+    
     /// <summary>
     /// 
     /// </summary>
